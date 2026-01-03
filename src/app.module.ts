194e4b4c2ffe1e5fd854219e335c2cc3e402b788
UrpirioDev <urpiriojunior@gmail.com>
2026-01-02 21:33:35 -0400

    Update

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,11 +18,11 @@ import { env } from 'env/env';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.MYSQLHOST ?? 'localhost',
-      port: parseInt(`${process.env.MYSQLPORT}`) ?? 3006,
-      username: env.MYSQLUSER ?? 'root',
-      password: process.env.MYSQLPASSWORD ?? 'PepeApp12@',
-      database: process.env.MYSQLDATABASE  ?? 'test-nestjs',
+      host: "mysql.railway.internal",
+      port: 3006,
+      username: 'root',
+      password: "DIXyTKsVdTwzwtoTUWNJnWgajsilXLbY",
+      database: "railway",
       autoLoadEntities: true,
       synchronize: true,
     }),
