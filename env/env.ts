753b026e553dd21334d5928e7a9bffddc7c882a9
UrpirioDev <urpiriojunior@gmail.com>
2026-01-02 21:21:38 -0400

    Se agregaron variable de entornos

---
+++ env/env.ts
@@ -0,0 +1,8 @@
+export const env = {
+  MYSQLHOST: process.env.MYSQLHOST ,
+  MYSQL_ROOT_PASSWORD: process.env.MYSQL_ROOT_PASSWORD ,
+  MYSQLDATABASE: process.env.MYSQLDATABASE ,
+  MYSQLPASSWORD: process.env.MYSQLPASSWORD ,
+  MYSQLPORT: process.env.MYSQLPORT,
+  MYSQLUSER: process.env.MYSQLUSER,
+};
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,25 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
 import { UsersModule } from './users/users.module';
 import { ServiciosModule } from './servicios/servicios.module';
+import { env } from 'env/env';
 
+//  type: 'mysql',
+//       host: env.MYSQLHOST,
+//       port: parseInt(`${env.MYSQLPORT}`),
+//       username: env.MYSQLUSER,
+//       password: env.MYSQLPASSWORD,
+//       database: env.MYSQLDATABASE,
+//       autoLoadEntities: true,
+//       synchronize: true,
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: "PepeApp12@",
-      database: 'test-nestjs',
+      host: process.env.MYSQLHOST ?? 'localhost',
+      port: parseInt(`${process.env.MYSQLPORT}`) ?? 3006,
+      username: env.MYSQLUSER ?? 'root',
+      password: process.env.MYSQLPASSWORD ?? 'PepeApp12@',
+      database: process.env.MYSQLDATABASE  ?? 'test-nestjs',
       autoLoadEntities: true,
       synchronize: true,
     }),
