0775c7fa44835efd9cf2cd7c2370954649a04c2d
UrpirioDev <urpiriojunior@gmail.com>
2026-01-02 20:50:02 -0400

    Se agrego la conexion a la base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
 import { ProductosModule } from './productos/productos.module';
-// import { DataSource } from 'typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { UsersModule } from './users/users.module';
+import { ServiciosModule } from './servicios/servicios.module';
 
 @Module({
-  imports: [ProductosModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: "PepeApp12@",
+      database: 'test-nestjs',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    ProductosModule,
+    UsersModule,
+    ServiciosModule,
+  ],
 })
 export class AppModule {
-  // constructor(private dataSource: DataSource) {}
+  constructor(private dataSource: DataSource) {}
 }
