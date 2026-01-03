aaecef8cc1c2a1c7f16316766c596d9446e73811
UrpirioDev <urpiriojunior@gmail.com>
2026-01-02 20:49:27 -0400

    Se agrego  Usuarios con los constroladores, servicios, modulos y entidad

---
+++ src/users/users.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private UserRepository: Repository<User>,
+  ) {}
+  async create(createUserDto: CreateUserDto) {
+    const user = this.UserRepository.create(createUserDto);
+    return await this.UserRepository.save(user);
+  }
+
+  async findAll() {
+    return await this.UserRepository.find();
+  }
+
+  async findOne(id: number) {
+    return await this.UserRepository.findOneBy({ id });
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    return await this.UserRepository.update(id, updateUserDto);
+  }
+
+  async remove(id: number) {
+    return await this.UserRepository.delete(id);
+  }
+}
